a151267a954265d77bb5020a8af64cdeeffbe849
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-06-10 04:13:41 -0400

    Remove import statement from env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,10 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
-import type { TimeSpanUnit } from 'oslo';
 
 declare namespace App {
   interface Locals {
-    session: import("lucia").Session | null;
-    user: import("lucia").User | null;
+    session: import('lucia').Session | null;
+    user: import('lucia').User | null;
   }
 }
 interface ImportMetaEnv {
