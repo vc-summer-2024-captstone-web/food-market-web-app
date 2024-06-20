884225fae9390d8dee75c71b95428d6e89abe741
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-06-20 19:22:01 -0400

    Add/seed example table and routes, update layout
    - Example table has id and itemname
    - Api end point

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,19 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
 // https://astro.build/db/config
+
+const ExampleTable = defineTable({
+  columns: {
+    Id: column.number({
+      optional: false,
+      primaryKey: true
+    }),
+    ItemName: column.text()
+  }
+});
+
 export default defineDb({
-  tables: {},
+  tables: {
+    ExampleTable
+  },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,10 @@
-import { db } from 'astro:db';
+import { db, ExampleTable } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
   // TODO
+  await db.insert(ExampleTable).values({
+    Id: 1,
+    ItemName: 'Burger'
+  })
 }
