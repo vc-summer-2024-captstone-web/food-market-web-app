90ca3f7d826ec334f3fb327d6294e58f7a29645d
anlujan <97326828+anlujan@users.noreply.github.com>
2024-06-30 22:53:34 -0400

    Update seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,26 @@
 import { db } from 'astro:db';
+import prisma, { encryptBody } from './config';
 
 // https://astro.build/db/seed
 export default async function seed() {
   // TODO
 }
+
+async function main() {
+  const emailLog = await prisma.emailLog.create({
+    data: {
+      body: encryptBody('Sample email body content here'),
+    },
+  });
+
+  console.log({ emailLog });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
