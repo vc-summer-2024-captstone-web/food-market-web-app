71038ba9a0065076bfcb1d4cefb3c152e3493060
anlujan <97326828+anlujan@users.noreply.github.com>
2024-06-30 22:08:09 -0400

    Email log table with encryption

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
 import { defineDb } from 'astro:db';
 
 // https://astro.build/db/config
+
+const ExampleTable = defineTable({
+    columns: {
+      ID: columnNumber({
+        optional: false,
+
+      
+      }),
+      ItemName: column.text()
+    }
+})
+
 export default defineDb({
-  tables: {},
+  tables: {
+    ExampleTable
+  },
 });
