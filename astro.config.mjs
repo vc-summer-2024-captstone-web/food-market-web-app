0091625c0c92745dd9983f493da98b1f1e1c7a17
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-07-08 13:53:39 -0400

    Install sentry and add to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-
 import netlify from '@astrojs/netlify';
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  integrations: [db(), sentry()],
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
