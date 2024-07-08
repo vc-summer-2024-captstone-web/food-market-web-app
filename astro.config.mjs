96b76fa57376d9103540ba68cc98287fba8fe5f9
Manuel Osorio <manuelosorio@users.noreply.github.com>
2024-07-08 14:51:41 -0400

    Install sentry and add to astro config (#59)
    * Install sentry and add to astro config
    
    * Configure sentry in astro.cofnig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-
 import netlify from '@astrojs/netlify';
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  integrations: [
+    db(),
+    sentry({
+      dsn: process.env.SENTRY_DNS,
+      sourceMapsUploadOptions: {
+        project: process.env.SENTRY_PROJECT_ID,
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }
+    })
+  ],
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
