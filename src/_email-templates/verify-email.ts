22a85b8cde87615eae7dbe855dbd9e53ec73c5a3
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-07-10 18:15:40 -0400

    Make sure verify alert is visible on all pages

--- src/_email-templates/verify-email.ts
+++ src/_email-templates/verify-email.ts
@@ -5,6 +5,7 @@ export const VerifyEmail = `
     <p>Welcome to the {{appName}}, {{name}}</p>
     <p>Your verification code is: <b>{{token}}</b></p>
     <p><a href='https://food-market-web-app.netlify.app/auth/verify'>Click here to verify your email</a></p>
+    <p>Or copy and paste this link into your browser: <br />https://food-market-web-app.netlify.app/auth/verify</p>
   </body>
 </html>
 `;
