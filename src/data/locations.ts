d0e4a4d72c8aaff062c8822fdb8d01f364e66d92
Raymond <ynoa.raymond@gmail.com>
2024-07-14 15:57:27 -0400

    Example of scrolling list

---
+++ src/data/locations.ts
@@ -0,0 +1,63 @@
+ export const locations = [
+    {
+        storeType: "Food Pantry 1",
+        distance: "5 miles",
+        openHours: "8:00 AM - 9:00 PM",
+        address: "123 Main St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 2",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 3",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 4",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 5",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 6",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 7",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 8",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 9",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      },
+      {
+        storeType: "Food Pantry 10",
+        distance: "3 miles",
+        openHours: "7:00 AM - 10:00 PM",
+        address: "456 Generic St, Orlando, FL"
+      }
+  ];
+  
