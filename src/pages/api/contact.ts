64e3b4638fe1224a6b00a2670dbf984978c398d9
Manuel Osorio <manuelosorio@users.noreply.github.com>
2024-07-18 17:39:33 -0400

    [Feature] Add and implement authorization (#67)
    * Create permission class and enum
    
    - Class checks for a role's permisssions.
    
    - The enum is used to easily call the hasPermission method
    
    - The hasPermission method returns a boolean for the specific permission from a role
    
    * Prevent unauthorized access to contact endpoint
    
    * Fix issues with contact form submission

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,7 +1,7 @@
 import type { APIContext } from 'astro';
-import { db, ContactFormLog } from 'astro:db';
+import { ContactFormLog, db, eq, User } from 'astro:db';
 
-import { encryptBody, decipherBody } from '@services';
+import { decipherBody, encryptBody, Permission, Permissions } from '@services';
 import { response } from '@utilities';
 import { createId } from '@paralleldrive/cuid2';
 
@@ -10,9 +10,26 @@ export async function POST(context: APIContext): Promise<Response> {
   const name = formData.get('name') as string;
   const message = formData.get('message') as string;
   const email = formData.get('email') as string;
+  const phone = formData.get('phone') as string;
+  if (!name || !message || !email || !phone) {
+    return response(
+      {
+        error: 'Missing required fields',
+        fields: {
+          name: !!name,
+          message: !!message,
+          email: !!email,
+          phone: !!phone,
+        },
+      },
+      400
+    );
+  }
+
   const emailBody = JSON.stringify({
     name,
     email,
+    phone,
     message,
   });
   const encryptedMessage = encryptBody(emailBody);
@@ -33,6 +50,29 @@ export async function POST(context: APIContext): Promise<Response> {
 }
 
 export async function GET(context: APIContext): Promise<Response> {
+  const session = context.locals.session;
+  if (!session) {
+    return response({ error: 'Unauthorized' }, 401);
+  }
+  const results = await db
+    .select({
+      id: User.id,
+      role: User.role,
+    })
+    .from(User)
+    .where(eq(User.id, session.userId));
+  const user = await results[0];
+  if (!user) {
+    return response({ error: 'Unauthorized' }, 401);
+  }
+  const permissions = new Permission(user.role);
+  if (!permissions) {
+    return response({ error: 'Unauthorized' }, 401);
+  }
+  const canViewLogs = await permissions.hasPermission(Permissions.canViewContactFormLogs);
+  if (!canViewLogs) {
+    return response({ error: 'Unauthorized' }, 401);
+  }
   const messages = await db.select().from(ContactFormLog).all();
   if (messages.length === 0) {
     return response({ message: 'No messages found' }, 404);
--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './email.service.ts';
 export * from './crypto.service.ts';
 export * from './verification.service.ts';
 export * from './roles.ts';
+export * from './permissions.ts';
