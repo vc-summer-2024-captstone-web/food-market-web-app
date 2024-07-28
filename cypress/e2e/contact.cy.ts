0adb667c47e62529b8c50b6f6fc04fb6b4e790a7
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-07-28 16:30:30 -0400

    Fix contact tests

--- cypress/e2e/contact.cy.ts
+++ cypress/e2e/contact.cy.ts
@@ -1,8 +1,9 @@
 it('Contact Use Page', () => {
+  cy.visit('http://localhost:4321/contact');
   cy.get('#name').type('TestUser');
-  cy.get('#number').type('1011011011');
+  cy.get('#phone').type('1011011011');
   cy.get('#email').type('testuser@example.com');
   cy.get('#message').type('Test Message');
   cy.get('#submit').click();
-  cy.contains('Messagese sent').should('be.visible');
+  cy.contains('Thank you for your message! We will get back to you soon.').should('be.visible');
 });
