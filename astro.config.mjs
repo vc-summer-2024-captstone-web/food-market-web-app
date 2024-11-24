3af82c7148ad90027251abeb874f68fc8945a11c
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-11-24 03:40:10 -0500

    Disable sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 import netlify from '@astrojs/netlify';
 
-import sentry from '@sentry/astro';
+// import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     db(),
-    sentry({
-      dsn: process.env.SENTRY_DNS,
-      sourceMapsUploadOptions: {
-        project: process.env.SENTRY_PROJECT_ID,
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
+    // sentry({
+    //   dsn: process.env.SENTRY_DNS,
+    //   sourceMapsUploadOptions: {
+    //     project: process.env.SENTRY_PROJECT_ID,
+    //     authToken: process.env.SENTRY_AUTH_TOKEN,
+    //   },
+    // }),
   ],
   output: 'server',
   adapter: netlify(),
