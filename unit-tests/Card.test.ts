0e06d1d19bf7bbcc8cf198cc9a819dee7bfbb154
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-11-23 20:09:41 -0500

    Add 'NourishMap' to remaining page titles

--- unit-tests/Card.test.ts
+++ unit-tests/Card.test.ts
@@ -1,6 +1,6 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, test } from 'vitest';
-import Card from '../src/components/Card.astro';
+import Card from '@components/Card.astro';
 
 test('Card with slots', async () => {
   const container = await AstroContainer.create();
