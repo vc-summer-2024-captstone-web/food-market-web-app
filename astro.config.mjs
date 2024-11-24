973c469dd52799ae32776fc8cb33501028250ccc
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-11-24 06:05:36 -0500

    Enable sentry client-side only

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,24 @@ import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 import netlify from '@astrojs/netlify';
 
-// import sentry from '@sentry/astro';
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     db(),
-    // sentry({
-    //   dsn: process.env.SENTRY_DNS,
-    //   sourceMapsUploadOptions: {
-    //     project: process.env.SENTRY_PROJECT_ID,
-    //     authToken: process.env.SENTRY_AUTH_TOKEN,
-    //   },
-    // }),
+    sentry({
+      dsn: process.env.SENTRY_DNS,
+      sourceMapsUploadOptions: {
+        project: process.env.SENTRY_PROJECT_ID,
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+      enabled: {
+        client: true,
+        //todo: find a solution to fix issues with drizzle-orm to re-enable server side sentry
+        server: false,
+      }
+    }),
   ],
   output: 'server',
   adapter: netlify(),
